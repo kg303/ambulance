a49cd2ab53a9476d7607bd308cfff9a884fb10e7
Dejan C <dejan.crnomarkovic@asioso.com>
2024-01-17 16:47:50 +0100

    Template segmentation.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,27 +2,24 @@
 
 namespace App\Controller;
 
-use Pimcore\Bundle\AdminBundle\Controller\Admin\LoginController;
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bridge\Twig\Attribute\Template;
 
 class DefaultController extends FrontendController
 {
-    /**
-     * @param Request $request
-     * @return Response
-     */
-    public function defaultAction(Request $request): Response
+
+    #[Template('content/main.html.twig')]
+    public function defaultAction(Request $request)
     {
-        return $this->render('layouts/layout.html.twig');
+        return [];
     }
 
-    /**
-     * Forwards the request to admin login
-     */
-    public function loginAction(): Response
+
+    #[Template('includes/footer.html.twig')]
+    public function footerAction(Request $request)
     {
-        return $this->forward(LoginController::class.'::loginCheckAction');
+        return [];
     }
 }
